c0aa31d6d8bfef9faf8d6c01a722605a3b862eab
chamil-dev <77248515+chamil-dev@users.noreply.github.com>
2023-12-29 11:08:19 +0530

    Update hello_web.sh

--- hello_web.sh
+++ hello_web.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt install apache2* -y
 sudo systemctl enable -now apach2
 sudo systemctl start apach2
-sudo cp /var/ww/html/index.html /var/ww/html/index.html_backup
+sudo cp /var/www/html/index.html /var/www/html/index.html_backup
 sudo -i
 echo "Test web" >> /var/www/html/index.html
 
